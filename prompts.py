9c5c737b65dcbabd7b76e861df0c7c802cc90cba
Rafael Cosman <rafaelcosman@alumni.stanford.edu>
2023-02-01 17:51:20 -0800

    moves prompt to a separate prompts.py file

---
+++ prompts.py
@@ -0,0 +1,78 @@
+import os
+
+def get_all_files():
+    # Get list of all files in the current directory
+    files = os.listdir('.')
+    all_files_appended = ""
+
+    # Iterate through each file
+    for file in files:
+        # Open and read the file
+        if os.path.isfile(file):
+            with open(file) as fp:
+                # Print the contents of the file
+
+                all_files_appended += "\n================\n"
+                all_files_appended += file
+                all_files_appended += "\n================\n"
+                all_files_appended += fp.read()
+                all_files_appended += "\n"
+
+    return all_files_appended
+
+all_files_appended = get_all_files()
+
+def prompt(input_from_user):
+    return f"""You are an experienced programmer that can write safe, readable, and commented code in a variety of languages.
+
+You are working in a directory with the following contents:
+
+{all_files_appended}
+
+Your goal now is to {input_from_user}.
+Make sure to write safe, readable, and commented code in an appropriate language based on the goal.
+
+
+================
+EXAMPLES
+================
+
+Code to write me a python file called example.py that prints out Hello World!:
+
+# example.py
+if __name__ == "__main__":
+    print("Hello World!")
+
+Code to Modify example.py to print it out 10 times:
+
+# example.py
+if __name__ == "__main__":
+    for i in range(10):
+        print("Hello World!")
+
+Code to please comment example.py:
+
+# example.py
+# This file prints out "Hello World!" 10 times
+
+if __name__ == "__main__": # Checks to see if the program is being run directly
+    for i in range(10):
+        print("Hello World!") # Prints "Hello World!" each time the loop is executed
+
+Code to please write me a command line tool called autocoder:
+# autocoder
+#!/usr/bin/env python3
+
+print("Welcome to autocoder!")
+
+Code to go ahead and run the webserver:
+```console
+python3 webserver.py
+```
+
+
+================
+Actual Prompt
+================
+
+Code to {input_from_user}:"""
